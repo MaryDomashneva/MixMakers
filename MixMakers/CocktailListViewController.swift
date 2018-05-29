bec32161623174262d6db0ab53e8c1383f4d6d2c
marydomashneva <mary.domashneva@gmail.com>
2018-05-29 22:20:16 +0100

    Connect CoctailListViewController with Screen

---
+++ MixMakers/CocktailListViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CocktailListViewController.swift
+//  MixMakers
+//
+//  Created by Maria Getmanova on 29/05/2018.
+//  Copyright © 2018 MixMakers. All rights reserved.
+//
+
+import UIKit
+
+class CocktailListViewController: UIViewController
+{
+    @IBOutlet var tableView: UITableView! //link code and view
+}
