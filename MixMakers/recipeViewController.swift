a5206fcbaad499f26fd6397d80893d8b928cd96f
Louis Moselhi <moselhil@gmail.com>
2018-05-31 15:59:46 +0100

    in middle of third screen dev

---
+++ MixMakers/recipeViewController.swift
@@ -0,0 +1,40 @@
+//
+//  recipeViewController.swift
+//  MixMakers
+//
+//  Created by Louis Moselhi on 31/05/2018.
+//  Copyright © 2018 MixMakers. All rights reserved.
+//
+
+import UIKit
+
+class recipeViewController: UIViewController {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var ingredients: UILabel!
+    @IBOutlet weak var instructions: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
