c48ef752078e6355ec5d05d24c98d021c3ad0c4d
Andrew Kemp <akemp850@gmail.com>
2018-06-02 15:56:31 +0100

    recipe screen styled

--- MixMakers/Models/CocktailRecipes.swift
+++ MixMakers/Models/CocktailRecipes.swift
@@ -9,16 +9,38 @@
 import Foundation
 
 struct CocktailRecipes:Decodable {
-    let strInstructions: String
-    let strIngredient1: String
-    let strIngredient2: String
-    let strIngredient3: String
-    let strIngredient4: String
-    let strDrinkThumb: String
-    let strMeasure1: String
-    let strMeasure2: String
-    let strMeasure3: String
-    let strMeasure4: String
+    let strInstructions: String?
+    let strIngredient1: String?
+    let strIngredient2: String?
+    let strIngredient3: String?
+    let strIngredient4: String?
+    let strIngredient5: String?
+    let strIngredient6: String?
+    let strIngredient7: String?
+    let strIngredient8: String?
+    let strIngredient9: String?
+    let strIngredient10: String?
+    let strIngredient11: String?
+    let strIngredient12: String?
+    let strIngredient13: String?
+    let strIngredient14: String?
+    let strIngredient15: String?
+    let strDrinkThumb: String?
+    let strMeasure1: String?
+    let strMeasure2: String?
+    let strMeasure3: String?
+    let strMeasure4: String?
+    let strMeasure5: String?
+    let strMeasure6: String?
+    let strMeasure7: String?
+    let strMeasure8: String?
+    let strMeasure9: String?
+    let strMeasure10: String?
+    let strMeasure11: String?
+    let strMeasure12: String?
+    let strMeasure13: String?
+    let strMeasure14: String?
+    let strMeasure15: String?
     let id: String
     let strDrink: String
     
@@ -29,12 +51,80 @@ struct CocktailRecipes:Decodable {
         case strIngredient2 = "strIngredient2"
         case strIngredient3 = "strIngredient3"
         case strIngredient4 = "strIngredient4"
+        case strIngredient5 = "strIngredient5"
+        case strIngredient6 = "strIngredient6"
+        case strIngredient7 = "strIngredient7"
+        case strIngredient8 = "strIngredient8"
+        case strIngredient9 = "strIngredient9"
+        case strIngredient10 = "strIngredient10"
+        case strIngredient11 = "strIngredient11"
+        case strIngredient12 = "strIngredient12"
+        case strIngredient13 = "strIngredient13"
+        case strIngredient14 = "strIngredient14"
+        case strIngredient15 = "strIngredient15"
         case strDrinkThumb = "strDrinkThumb"
         case strMeasure1 = "strMeasure1"
         case strMeasure2 = "strMeasure2"
         case strMeasure3 = "strMeasure3"
         case strMeasure4 = "strMeasure4"
+        case strMeasure5 = "strMeasure5"
+        case strMeasure6 = "strMeasure6"
+        case strMeasure7 = "strMeasure7"
+        case strMeasure8 = "strMeasure8"
+        case strMeasure9 = "strMeasure9"
+        case strMeasure10 = "strMeasure10"
+        case strMeasure11 = "strMeasure11"
+        case strMeasure12 = "strMeasure12"
+        case strMeasure13 = "strMeasure13"
+        case strMeasure14 = "strMeasure14"
+        case strMeasure15 = "strMeasure15"
         case id = "idDrink"
         case strDrink = "strDrink"
+    }
+    
+    func ingredients() -> [String] {
+        var result: [String] = []
+        append(value: strIngredient1, to: &result)
+        append(value: strIngredient2, to: &result)
+        append(value: strIngredient3, to: &result)
+        append(value: strIngredient4, to: &result)
+        append(value: strIngredient5, to: &result)
+        append(value: strIngredient6, to: &result)
+        append(value: strIngredient7, to: &result)
+        append(value: strIngredient8, to: &result)
+        append(value: strIngredient9, to: &result)
+        append(value: strIngredient10, to: &result)
+        append(value: strIngredient11, to: &result)
+        append(value: strIngredient12, to: &result)
+        append(value: strIngredient13, to: &result)
+        append(value: strIngredient14, to: &result)
+        append(value: strIngredient15, to: &result)
+        return result
+    }
+    
+    func measures() -> [String] {
+        var result: [String] = []
+        append(value: strMeasure1, to: &result)
+        append(value: strMeasure2, to: &result)
+        append(value: strMeasure3, to: &result)
+        append(value: strMeasure4, to: &result)
+        append(value: strMeasure5, to: &result)
+        append(value: strMeasure6, to: &result)
+        append(value: strMeasure7, to: &result)
+        append(value: strMeasure8, to: &result)
+        append(value: strMeasure9, to: &result)
+        append(value: strMeasure10, to: &result)
+        append(value: strMeasure11, to: &result)
+        append(value: strMeasure12, to: &result)
+        append(value: strMeasure13, to: &result)
+        append(value: strMeasure14, to: &result)
+        append(value: strMeasure15, to: &result)
+        return result
+    }
+    
+    func append(value: String?, to array: inout [String]) {
+        if let value = value, !value.isEmpty {
+            array.append(value)
+        }
     }
 }
--- MixMakers/Recipe/recipeViewController.swift
+++ MixMakers/Recipe/recipeViewController.swift
@@ -7,17 +7,38 @@
 //
 
 import UIKit
+import Nuke
 
-class recipeViewController: UIViewController {
+class recipeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    @IBOutlet weak var image: UIImageView!
-    @IBOutlet weak var ingredients: UILabel!
-    @IBOutlet weak var instructions: UILabel!
-    @IBOutlet weak var measure: UILabel!
+    enum TableViewSection: Int {
+        case Ingredients = 0
+        case Instructions = 1
+        
+        func name() -> String {
+            switch self {
+            case .Instructions:
+                return "Instructions"
+            case .Ingredients:
+                return "Ingredients"
+            }
+        }
+        
+        func height() -> CGFloat {
+            return 60
+        }
+        
+        static func numberOfSections() -> Int {
+            return 2
+        }
+    }
+    
+    @IBOutlet weak var cocktailImageViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var cocktailImage: UIImageView!
+    @IBOutlet weak var recipeTableView: UITableView!
     
     var cocktail:SimpleCocktail?
-    
-    var recipe: [CocktailRecipes] = []
+    var recipe: CocktailRecipes?
     let recipeAPIService = RecipeService()
     
     
@@ -25,6 +46,11 @@ class recipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        recipeTableView.rowHeight = UITableViewAutomaticDimension
+        recipeTableView.estimatedRowHeight = 44
+        recipeTableView.register(UINib(nibName: "RecipeInstructionTableViewCell", bundle: nil), forCellReuseIdentifier: "RecipeInstructionTableViewCell")
+        recipeTableView.register(UINib(nibName: "RecipeIngredientTableViewCell", bundle: nil), forCellReuseIdentifier: "RecipeIngredientTableViewCell")
+        recipeTableView.register(UINib(nibName: "RecipeHeaderView", bundle: nil), forHeaderFooterViewReuseIdentifier: "RecipeHeaderView")
         
         loadRecipe(with: (cocktail?.id)!)
 
@@ -51,13 +77,67 @@ class recipeViewController: UIViewController {
     }
     
     func update(with newRecipe: [CocktailRecipes]) {
-        recipe = newRecipe
-        ingredients.text = recipe[0].strIngredient1
-        instructions.text = recipe[0].strInstructions
-        measure.text = recipe[0].strMeasure1
+        recipe = newRecipe.first
+        if let strDrinkThumb = recipe?.strDrinkThumb, let imageURL = URL(string: strDrinkThumb) {
+            Manager.shared.loadImage(with: imageURL, into: cocktailImage)
+        }
+        recipeTableView.reloadData()
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return TableViewSection.numberOfSections()
+    }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch TableViewSection.init(rawValue: section)! {
+        case .Ingredients:
+            return recipe?.ingredients().count ?? 0
+        case .Instructions:
+            return recipe?.strInstructions != nil ? 1 : 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if recipe == nil {
+            return nil
+        }
+        let tableViewSection = TableViewSection.init(rawValue: section)!
+        let headerView = tableView.dequeueReusableHeaderFooterView(withIdentifier: "RecipeHeaderView") as! RecipeHeaderView
+        headerView.titleLabel.text = tableViewSection.name()
+        return headerView
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let tableViewSection = TableViewSection.init(rawValue: section)!
+        return tableViewSection.height()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch TableViewSection.init(rawValue: indexPath.section)! {
+        case .Ingredients:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeIngredientTableViewCell", for: indexPath) as! RecipeIngredientTableViewCell
+            let ingredient = recipe?.ingredients()[indexPath.row]
+            let measure = recipe?.measures()[indexPath.row]
+            cell.ingredientLabel.text = ingredient
+            cell.measureLabel.text = measure
+            return cell
+            
+        case .Instructions:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "RecipeInstructionTableViewCell", for: indexPath) as! RecipeInstructionTableViewCell
+            let instruction = recipe?.strInstructions
+            cell.instructionLabel.text = instruction
+            return cell
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        if offsetY >= 0 {
+            cocktailImageViewHeight.constant = 150
+        } else {
+            cocktailImageViewHeight.constant = 150 - offsetY
+        }
+    }
     
     /*
     // MARK: - Navigation
