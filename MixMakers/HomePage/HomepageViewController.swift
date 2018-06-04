f52aba654d8d5a276a80d98a36d87c406fec6b64
Jack Dewinter <jackdewinter1@gmail.com>
2018-06-04 14:20:15 +0100

    invisible buttons for results

--- MixMakers/HomePage/HomepageViewController.swift
+++ MixMakers/HomePage/HomepageViewController.swift
@@ -1,22 +1,40 @@
-//
-//  HomepageViewController.swift
-//  MixMakers
-//
-//  Created by Jack Dewinter on 30/05/2018.
-//  Copyright © 2018 MixMakers. All rights reserved.
-//
-
 import UIKit
 import SearchTextField
 
 class HomepageViewController: UIViewController {
-
-
+    
+    var searchTerm: [String] = []
     
     
     @IBOutlet weak var searchIngredientsText: SearchTextField!
     
-
+    @IBOutlet weak var ingredient1: UIButton!
+    @IBOutlet weak var ingredient2: UIButton!
+    @IBOutlet weak var ingredient3: UIButton!
+    
+    @IBAction func addPressed(_ sender: Any) {
+        if !(searchIngredientsText.text?.isEmpty)! {
+            if searchTerm.count == 0 {
+                searchTerm.append(searchIngredientsText.text!)
+                ingredient1.setTitle(searchIngredientsText.text, for: .normal)
+                ingredient1.isHidden = false
+            }
+            else if searchTerm.count == 1 {
+                searchTerm.append(searchIngredientsText.text!)
+                ingredient2.setTitle(searchIngredientsText.text, for: .normal)
+                ingredient2.isHidden = false
+            }
+                
+            else if searchTerm.count == 2 {
+                searchTerm.append(searchIngredientsText.text!)
+                ingredient3.setTitle(searchIngredientsText.text, for: .normal)
+                ingredient3.isHidden = false
+            }
+            
+        }
+        
+    }
+    
     @IBAction func searchPressed(_ sender: Any) {
         if let searchText = searchIngredientsText.text, !searchText.isEmpty {
             let resultView = storyboard?.instantiateViewController(withIdentifier: "Cocktails") as! CocktailListViewController
@@ -38,7 +56,6 @@ class HomepageViewController: UIViewController {
     }
     
     func configureSearchField() {
-        searchIngredientsText.startVisibleWithoutInteraction = true
         searchIngredientsText.filterStrings(["Vodka", "Gin", "Tequila"])
     }
     
@@ -47,5 +64,5 @@ class HomepageViewController: UIViewController {
         super.viewDidLayoutSubviews()
         
     }
-
+    
 }
