591d713772d7bd695ef76840cab14425eea6ee4d
Jack Dewinter <jackdewinter1@gmail.com>
2018-06-04 15:03:57 +0100

    buttons can be manipulated for search

--- MixMakers/HomePage/HomepageViewController.swift
+++ MixMakers/HomePage/HomepageViewController.swift
@@ -18,32 +18,29 @@ class HomepageViewController: UIViewController {
             if searchTerm.count == 0 {
                 searchTerm.append(searchString!)
                 ingredient1.setTitle(searchString, for: .normal)
-                ingredient1.isHidden = false
             }
             else if searchTerm.count == 1 {
                 searchTerm.append(searchString!)
                 ingredient2.setTitle(searchString, for: .normal)
-                ingredient2.isHidden = false
             }
                 
             else if searchTerm.count == 2 {
                 searchTerm.append(searchString!)
                 ingredient3.setTitle(searchString, for: .normal)
-                ingredient3.isHidden = false
-                searchIngredientsText.isEnabled = false
                 
             }
         }
         searchIngredientsText.text = ""
+        
     }
     
     @IBAction func searchPressed(_ sender: Any) {
-        if let searchText = searchIngredientsText.text, !searchText.isEmpty {
+        if let searchText = searchIngredientsText.text, !searchTerm.isEmpty {
             let resultView = storyboard?.instantiateViewController(withIdentifier: "Cocktails") as! CocktailListViewController
-            resultView.searchTerm.append(searchIngredientsText.text!)
+            resultView.searchTerm.append(contentsOf: searchTerm)
             navigationController?.pushViewController(resultView, animated: true)
         } else {
-            let emptySearchAlert = UIAlertController(title: "Please, enter ingredient!", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            let emptySearchAlert = UIAlertController(title: "Please, enter an ingredient!", message: nil, preferredStyle: UIAlertControllerStyle.alert)
             let okButton = UIAlertAction(title: "Ok", style: UIAlertActionStyle.cancel, handler: nil)
             emptySearchAlert.addAction(okButton)
             present(emptySearchAlert, animated: true, completion: nil)
@@ -51,6 +48,38 @@ class HomepageViewController: UIViewController {
     }
     
     
+    @IBAction func ingredient1Pressed(_ sender: Any) {
+        searchTerm.remove(at: searchTerm.index(of: ingredient1.title(for: .normal)!)!)
+        if ingredient2.title(for: .normal) == "" {
+            ingredient1.setTitle("", for: .normal)
+        }
+        else {
+            ingredient1.setTitle(ingredient2.title(for: .normal), for: .normal)
+            if ingredient3.title(for: .normal) == "" {
+                ingredient2.setTitle("", for: .normal)
+            }
+            else {
+                ingredient2.setTitle(ingredient3.title(for: .normal), for: .normal)
+                ingredient3.setTitle("", for: .normal)
+            }
+        }
+    }
+    
+    @IBAction func ingredient2Pressed(_ sender: Any) {
+        searchTerm.remove(at: searchTerm.index(of: ingredient2.title(for: .normal)!)!)
+        if ingredient3.title(for: .normal) == "" {
+            ingredient2.setTitle("", for: .normal)
+        }
+        else {
+            ingredient2.setTitle(ingredient3.title(for: .normal), for: .normal)
+            ingredient3.setTitle("", for: .normal)
+        }
+    }
+    
+    @IBAction func ingredient3Pressed(_ sender: Any) {
+        searchTerm.remove(at: searchTerm.index(of: ingredient3.title(for: .normal)!)!)
+        ingredient3.setTitle("", for: .normal)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,7 +93,7 @@ class HomepageViewController: UIViewController {
     // Called whenever screen is going to be rendered
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        
+
     }
     
 }
