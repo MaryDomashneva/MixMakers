7f6e0a19ae8d3de094e8dd155695d9df49b106ed
Louis Moselhi <moselhil@gmail.com>
2018-06-05 14:33:49 +0100

    added another test file to check things are working

---
+++ MixMakersTests/testfile.swift
@@ -0,0 +1,11 @@
+//
+//  testfile.swift
+//  MixMakersTests
+//
+//  Created by Louis Moselhi on 05/06/2018.
+//  Copyright © 2018 MixMakers. All rights reserved.
+//
+
+import Foundation
+
+var yo = String("Hi there")
